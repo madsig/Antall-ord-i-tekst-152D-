b8cafe3929220290af44b4419612e2755faed3dc
madsig <madsig@getacademy.no>
2023-08-30 13:00:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,3 +46,11 @@ function countText() {
         text += words[i] + ': ' + wordCounts[i] + '<br>';
     }
 }
+
+//Oppgave 152 E
+function isListSorted(list) {
+    for (let i = 0; i < list.length - 1; i++) {
+        if (list[i] > list[i+1]) return false;
+    }
+    return true;
+}
